f445ece3c1e848c479c2a44155861e07b2d51c37
egorkharaim <egor.kharaima@gmail.com>
2026-01-10 20:53:05 +0300

    missing one space. incorrect level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-   private final Random random = new Random();
+    
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        // [CHECKLIST #2] Возвращаем значение сразу, без промежуточной переменной randomIndex
+
         return Colors.values()[random.nextInt(Colors.values().length)].name();
     }
 }
